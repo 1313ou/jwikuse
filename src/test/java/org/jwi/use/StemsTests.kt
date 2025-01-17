ecb4510cfe2501c284fbcca16884907e7396ff0f
1313ou <1313ou@gmail.com>
2025-01-17 07:55:07 +0100

    stemmer

--- src/test/java/org/jwi/use/StemsTests.kt
+++ src/test/java/org/jwi/use/StemsTests.kt
@@ -1,6 +1,8 @@
 package org.jwi.use
 
+import edu.mit.jwi.item.POS
 import edu.mit.jwi.morph.SimpleStemmer
+import edu.mit.jwi.morph.SimpleStemmer.Companion.cartesianProduct
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import java.io.IOException
@@ -9,16 +11,33 @@ import java.io.PrintStream
 class StemsTests {
 
     @Test
-    fun lemmasToMembers() {
-        stems(jwi, "works", "does", "finished", "boatsful", "hearts breakers")
+    fun cartesianProductTest() {
+        val lists = listOf("ABC".toList(), "123".toList(), "abc".toList())
+        val product = cartesianProduct(lists)
+        product.forEach { println(it.joinToString(separator = "")) }
+    }
+
+    @Test
+    fun wordsToStemsTestVerb() {
+        stems(POS.VERB, "works", "does", "finished")
+    }
+
+    @Test
+    fun wordsToStemsTestNoun() {
+        stems(POS.VERB, "works", "does", "boatsful", "hearts breakers")
+    }
+
+    @Test
+    fun wordsToStemsTestAll() {
+        stems(null, "works", "does", "finished", "boatsful", "hearts breakers")
     }
 
     companion object {
 
-        private fun stems(jwi: JWI, vararg words: String) {
+        private fun stems(pos: POS? = null, vararg words: String) {
             words.forEach {
                 val lemmas = stemmer.findStems(it, null)
-                PS.println("stems of $it are ${lemmas.joinToString(separator = ",\n\t", prefix = "\n\t")}")
+                PS.println("stems of '$it' for POS $pos are ${lemmas.joinToString(separator = "\n\t", prefix = "\n\t")}")
             }
         }
 
